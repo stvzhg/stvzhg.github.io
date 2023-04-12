a31a700e0d5355ba9df1fe31cc77767f001d635a
Steven Zhang <stvzhg@gmail.com>
2023-04-11 20:51:23 -0700

    ts lint

--- utilities/Autocomplete.ts
+++ utilities/Autocomplete.ts
@@ -53,24 +53,24 @@ export function activate(context: vscode.ExtensionContext) {
 class IncludeCompletionProvider implements vscode.CompletionItemProvider, vscode.Disposable {
 	private titles: string[] = [];
 	private watcher: vscode.FileSystemWatcher;
-	
+
 	constructor() {
 		this.updateTitles();
 
 		this.watcher = vscode.workspace.createFileSystemWatcher("**/_site/Autocomplete.txt");
-		this.watcher.onDidCreate(()=> this.updateTitles());
-		this.watcher.onDidChange(()=> this.updateTitles());
-		this.watcher.onDidDelete(()=> this.updateTitles());
+		this.watcher.onDidCreate(() => this.updateTitles());
+		this.watcher.onDidChange(() => this.updateTitles());
+		this.watcher.onDidDelete(() => this.updateTitles());
 	}
 
 	public dispose() {
 		this.watcher.dispose();
 	}
 
-	public provideCompletionItems (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+	public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
 		let linePrefix = document.lineAt(position).text.substr(0, position.character);
 		if (!linePrefix.endsWith('[[')) {
-				return undefined;
+			return undefined;
 		}
 
 		let completionItemArray: vscode.CompletionItem[] = [];
@@ -90,9 +90,9 @@ class IncludeCompletionProvider implements vscode.CompletionItemProvider, vscode
 		const folderUri = vscode.workspace.workspaceFolders[0].uri;
 		const fileUri = folderUri.with({ path: path.posix.join(folderUri.path, '_site/autocomplete.txt') });
 
-		let titles = <string[]> undefined;
+		let titles = <string[]>undefined;
 
-		const readData =  await vscode.workspace.fs.readFile(fileUri);
+		const readData = await vscode.workspace.fs.readFile(fileUri);
 		titles = Buffer.from(readData).toString('utf8').split(";");
 
 		this.titles = titles;
